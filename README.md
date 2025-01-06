789f52e23b3f37c19f1898a2cd0672719292f0dd
yadav54321 <git@stackblitz.com>
2025-01-06 10:10:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# counter-app
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/yadav54321/counter-app)
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './style.css';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
